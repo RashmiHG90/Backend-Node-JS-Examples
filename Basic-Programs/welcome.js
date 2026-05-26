455d7819deba8710939c7a5fb5f2780c1398a87d
RashmiHG90 <rashmi.hguk@gmail.com>
2026-05-26 08:32:22 +0100

    sample backend code

---
+++ Basic-Programs/welcome.js
@@ -0,0 +1,9 @@
+console.log("Welcome to JavaScript programming!");
+let id = 101;
+let name = "John Doe";
+let isActive = true;
+let hobbies = ["Reading", "Traveling", "Cooking"];
+console.log("ID:", id);
+console.log("Name:", name);
+console.log("Active:", isActive);
+console.log("Hobbies:", hobbies);
