839710472a533ff5c1f697b4d5b3cf2e051c0811
RashmiHG90 <rashmi.hguk@gmail.com>
2026-05-26 15:07:29 +0100

    ES6 & ES5 style changes

--- FS-module/writeSynchronous.js
+++ FS-module/writeSynchronous.js
@@ -1,8 +1,11 @@
-let fs = require('fs');
+//let fs = require('fs');
+//import is ES6 style, require is ES5 style. We are using ES6 style here.
+import { writeFileSync } from 'fs';
 console.log("Module loaded successfully!");
 let data = "This is a sample text to be written to the file.";
 try {
-    fs.writeFileSync('output.txt', data);
+    //fs.writeFileSync('output.txt', data);
+    writeFileSync('output.txt', data);
     console.log("Data written to file successfully!");
 } catch (err) {
     console.error("Error writing to file:", err);
