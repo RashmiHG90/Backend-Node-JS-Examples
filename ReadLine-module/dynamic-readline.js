f6a5c55c6232c3bba423fa2cb52ea5194fcd861a
RashmiHG90 <rashmi.hguk@gmail.com>
2026-06-07 08:17:37 +0100

    more example on readline

--- ReadLine-module/dynamic-readline.js
+++ ReadLine-module/dynamic-readline.js
@@ -1,11 +1,36 @@
 let obj = require('readline');
 
-let readline = obj.createInterface({
+// let readline = obj.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+
+// readline.question("Please enter your name:",(name)=>{
+//     console.log("Hello! welcome to node js, " + name);
+//     readline.question("Please enter your age:",(age)=>{
+//         console.log("Your age is: " + age);
+//         readline.question("Please enter your city:",(city)=>{
+//             console.log("You are from: " + city);
+//             readline.close();
+//         })
+//     })
+// })
+
+let rl = obj.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-readline.question("Please enter your name:",(name)=>{
-    console.log("Hello! welcome to node js, " + name);
-    readline.close();
-})
+rl.question("Please enter your id:",(id)=>{
+
+    rl.question("Please enter your name:",(name)=>{
+
+        rl.question("Please enter your age:",(age)=>{   
+
+        console.log("Your id is: " + id);
+        console.log("Your name is: " + name);          
+        console.log("Your age is: " + age); 
+        rl.close();     
+    });
+  });
+});
