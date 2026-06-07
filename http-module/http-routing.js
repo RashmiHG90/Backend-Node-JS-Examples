db2e25430423eb127aee5ae9f2150915e31fc43d
RashmiHG90 <rashmi.hguk@gmail.com>
2026-06-07 13:07:38 +0100

    added http-routing feature of url module with more examples

--- http-module/http-routing.js
+++ http-module/http-routing.js
@@ -4,15 +4,22 @@ let url = require('url');
 let server = http.createServer((request, response) =>{
 
     let urlRef = url.parse(request.url, true);
+    response.writeHead(200, {"Content-Type": "text/html"});
 
     if(urlRef.pathname == "/"){
         response.end("Welcome to http node JS module");
     }if(urlRef.pathname == "/about"){
-        response.end("This is about page");
+        // response.end("This is about page");
+        response.write("<h2> About Us Page </h2>");
+        response.write("<p> About us page description </p>");
     }else if(urlRef.pathname == "/contact"){
-        response.end("This is contact page");
+       // response.end("This is contact page");
+        response.write("<h2> Contact Us Page </h2>");
+        response.write("<p> Contact us page description </p>");
     }else if(urlRef.pathname == "/login"){
-        response.end("This is login page");
+        //response.end("This is login page");
+        response.write("<h2> Login Page </h2>");
+        response.write("<p> Login page description </p>");
     }else{
         response.end("Page not found");
     }
