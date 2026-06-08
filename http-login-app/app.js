49b92a5912f07f547f85fde5ca28ae0885649866
RashmiHG90 <rashmi.hguk@gmail.com>
2026-06-08 04:34:26 +0100

    composed small app with http, url-routing and fs to display html files

--- http-login-app/app.js
+++ http-login-app/app.js
@@ -10,19 +10,37 @@ let server = http.createServer((request, response) =>{
     if(urlRef.pathname == "/login"){
         let loginpage = fs.readFileSync("login.html");
         response.write(loginpage);
+        response.end();
         
     }else if(urlRef.pathname == "/aboutUs"){
         let aboutpage = fs.readFileSync("aboutus.html");
         response.write(aboutpage);
+        response.end();
+    }else if(urlRef.pathname=="/checkLogin"){
+        console.log(urlRef.query);
+        let login =urlRef.query;
+        let email =login.emailId;
+        let password = login.password;
+        // Add login validation logic here
+        console.log(email, password);
+        if(email == "raj@gmail.com" && password == "raj@123"){
+            response.write("Login successful!");
+        } else {
+            response.write("Login failed!");
+        }
+        response.end();
     }
     else if(urlRef.pathname == "/contactUs"){
         let contactpage = fs.readFileSync("contactus.html");
         response.write(contactpage);
+        response.end();
     }
     else{
         let indexpage = fs.readFileSync("index.html");
         response.write(indexpage);
+        response.end();
     }
+    response.end();
 
 })
 
