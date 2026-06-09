6b6239e0f9b592cf38a9723d27bcbfa61cf00303
RashmiHG90 <rashmi.hguk@gmail.com>
2026-06-09 14:37:48 +0100

    Added Get and Post methods using query, request on etc.

--- http-login-app/app.js
+++ http-login-app/app.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let url = require('url');
 let fs = require('fs');
+let queryString = require('querystring');
 let PORT = 3000;
 
 let server = http.createServer((request, response) =>{
@@ -17,6 +18,7 @@ let server = http.createServer((request, response) =>{
         response.write(aboutpage);
         response.end();
     }else if(urlRef.pathname=="/checkLogin"){
+        if(request.method == "GET"){
         console.log(urlRef.query);
         let login =urlRef.query;
         let email =login.emailId;
@@ -25,10 +27,36 @@ let server = http.createServer((request, response) =>{
         console.log(email, password);
         if(email == "raj@gmail.com" && password == "raj@123"){
             response.write("Login successful!");
+            response.end();
         } else {
             response.write("Login failed!");
+            response.end();
         }
-        response.end();
+      } else if(request.method == "POST"){
+        console.log(urlRef.query);        
+        let bodyData = "";
+        request.on("data",(formData)=>{
+            bodyData = bodyData+formData.toString();
+           // console.log(bodyData)
+        })
+        request.on("end",()=>{
+            console.log(bodyData);
+            let loginData = queryString.parse(bodyData);
+             let email = loginData.emailId;
+             let password = loginData.password;
+             console.log(loginData.emailId, loginData.password);
+             // Add login validation logic here
+             if(email == "raj@gmail.com" && password == "raj@123"){
+                console.log("Login successful!");
+                response.write("Login successful!");
+             } else {
+                response.write("Login failed!");
+             }
+             response.end();
+        })
+        // response.write("Post Form request received");
+        // response.end();
+      }
     }
     else if(urlRef.pathname == "/contactUs"){
         let contactpage = fs.readFileSync("contactus.html");
@@ -40,8 +68,6 @@ let server = http.createServer((request, response) =>{
         response.write(indexpage);
         response.end();
     }
-    response.end();
-
 })
 
 
